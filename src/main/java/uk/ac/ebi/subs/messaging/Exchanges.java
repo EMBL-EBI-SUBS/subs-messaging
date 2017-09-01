2e94936878f0867b9bfa724b2536a5a84da3c50e
Karoly Erdos <karoly@ebi.ac.uk>
2017-09-01 16:30:10 +0100

    Make exchanges public to be able to access from other dependent services

--- src/main/java/uk/ac/ebi/subs/messaging/Exchanges.java
+++ src/main/java/uk/ac/ebi/subs/messaging/Exchanges.java
@@ -5,7 +5,7 @@ class Exchanges {
 
     private static final String USI_CONTRACT_VERSION = "usi-1";
 
-    static final String SUBMISSIONS = USI_CONTRACT_VERSION+":submission-exchange";
+    public static final String SUBMISSIONS = USI_CONTRACT_VERSION+":submission-exchange";
 
-    static final String DEAD_LETTER_EXCHANGE = USI_CONTRACT_VERSION+":dead-letter-exchange";
+    public static final String DEAD_LETTER_EXCHANGE = USI_CONTRACT_VERSION+":dead-letter-exchange";
 }
