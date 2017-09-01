72af2581116f8586c3009e9afa60d8985d09d7d7
Karoly Erdos <karoly@ebi.ac.uk>
2017-09-01 17:01:02 +0100

    Make queue names public again as we are using them another services

--- src/main/java/uk/ac/ebi/subs/messaging/Queues.java
+++ src/main/java/uk/ac/ebi/subs/messaging/Queues.java
@@ -14,36 +14,36 @@ public class Queues {
     public static final String SUBMISSION_DELETED_ROUTING_KEY = Topics.EVENT_SUBMISSION_DELETED;
 
 
-    static final String SUBMISSION_MONITOR = "usi-submission-monitor";
-    static final String SUBMISSION_MONITOR_ROUTING_KEY = Topics.EVENT_SUBMISSION_AGENT_RESULTS;
+    public static final String SUBMISSION_MONITOR = "usi-submission-monitor";
+    public static final String SUBMISSION_MONITOR_ROUTING_KEY = Topics.EVENT_SUBMISSION_AGENT_RESULTS;
 
     public static final String SUBMISSION_DISPATCHER = "usi-submission-dispatcher";
 
     public static final String SUBMISSION_PROCESSING_UPDATED_DISPATCHER_ROUTING_KEY = Topics.EVENT_SUBMISSION_PROCESSING_UPDATED;
 
-    static final String SUBMISSION_SUPPORTING_INFO_PROVIDED = "usi-submission-supp-inf-provided";
-    static final String SUBMISSION_SUPPORTING_INFO_PROVIDED_ROUTING_KEY = Topics.EVENT_SUBISSION_SUPPORTING_INFO_PROVIDED;
+    public static final String SUBMISSION_SUPPORTING_INFO_PROVIDED = "usi-submission-supp-inf-provided";
+    public static final String SUBMISSION_SUPPORTING_INFO_PROVIDED_ROUTING_KEY = Topics.EVENT_SUBISSION_SUPPORTING_INFO_PROVIDED;
 
 
-    static final String SUBMISSION_NEEDS_SAMPLE_INFO = "usi-submission-support-biosamples";
-    static final String SUBMISSION_NEEDS_SAMPLE_INFO_ROUTING_KEY = Topics.EVENT_SUBMISSION_NEEDS_SAMPLES;
+    public static final String SUBMISSION_NEEDS_SAMPLE_INFO = "usi-submission-support-biosamples";
+    public static final String SUBMISSION_NEEDS_SAMPLE_INFO_ROUTING_KEY = Topics.EVENT_SUBMISSION_NEEDS_SAMPLES;
 
-    static final String SUBMISSION_VALIDATOR = "usi-submission-validator";
-    static final String SUBMISSION_VALIDATOR_SUBMISSION_CREATED_ROUTING_KEY = Topics.EVENT_SUBMISSION_CREATED;
-    static final String SUBMISSION_VALIDATOR_SUBMISSION_UPDATED_ROUTING_KEY = Topics.EVENT_SUBMISSION_UPDATED;
+    public static final String SUBMISSION_VALIDATOR = "usi-submission-validator";
+    public static final String SUBMISSION_VALIDATOR_SUBMISSION_CREATED_ROUTING_KEY = Topics.EVENT_SUBMISSION_CREATED;
+    public static final String SUBMISSION_VALIDATOR_SUBMISSION_UPDATED_ROUTING_KEY = Topics.EVENT_SUBMISSION_UPDATED;
 
-    static final String BIOSAMPLES_AGENT = "usi-submission-agent-biosamples";
-    static final String ENA_AGENT = "usi-submission-agents-ena";
-    static final String AE_AGENT = "usi-submission-agents-arrayexpress";
-    static final String METABOLIGHTS_AGENT = "usi-submission-agents-metabolights";
+    public static final String BIOSAMPLES_AGENT = "usi-submission-agent-biosamples";
+    public static final String ENA_AGENT = "usi-submission-agents-ena";
+    public static final String AE_AGENT = "usi-submission-agents-arrayexpress";
+    public static final String METABOLIGHTS_AGENT = "usi-submission-agents-metabolights";
 
-    static final String ENA_SAMPLES_UPDATED = "usi-submission-agents-ena-samples-updated";
-    static final String AE_SAMPLES_UPDATED = "usi-submission-agents-ae-samples-updated";
-    static final String METABOLIGHTS_SAMPLES_UPDATED = "usi-submission-agents-metabolights-samples-updated";
-    static final String SAMPLES_UPDATED_ROUTING_KEY = Topics.EVENT_SAMPLES_UPDATED;
+    public static final String ENA_SAMPLES_UPDATED = "usi-submission-agents-ena-samples-updated";
+    public static final String AE_SAMPLES_UPDATED = "usi-submission-agents-ae-samples-updated";
+    public static final String METABOLIGHTS_SAMPLES_UPDATED = "usi-submission-agents-metabolights-samples-updated";
+    public static final String SAMPLES_UPDATED_ROUTING_KEY = Topics.EVENT_SAMPLES_UPDATED;
 
-    static final String SUBMISSION_DEAD_LETTER = "usi-submission-dead-letter";
-    static final String SUBMISSION_DEAD_LETTER_ROUTING_KEY = "#";
+    public static final String SUBMISSION_DEAD_LETTER = "usi-submission-dead-letter";
+    public static final String SUBMISSION_DEAD_LETTER_ROUTING_KEY = "#";
 
     /**
      * Build an instance of a {@link Queue} configured with a Dead Letter Exchange.
